672e88ebd90fb06cca55285dd8a8172badd6d730
Timeexile <249585013@qq.com>
2020-03-27 23:32:24 +0800

    finish county selection

--- app/src/main/java/com/example/coolweather/db/County.java
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -5,7 +5,25 @@ import org.litepal.crud.DataSupport;
 public class County extends DataSupport {
     private int id;
     private String countyName;
-    private int countyCode;
+    private String weatherId;
+    private int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
 
     public int getId() {
         return id;
@@ -23,11 +41,4 @@ public class County extends DataSupport {
         this.countyName = countyName;
     }
 
-    public int getCountyCode() {
-        return countyCode;
-    }
-
-    public void setCountyCode(int countyCode) {
-        this.countyCode = countyCode;
-    }
 }
